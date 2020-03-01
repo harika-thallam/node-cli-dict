121e418f734f24bee80782d0a7ddcd62d8387e28
harika thallam <tharika70@gmail.com>
2020-03-01 21:49:48 +0530

    created dummy method for handling input arguments

--- ProcessInput.js
+++ ProcessInput.js
@@ -1,1 +1,20 @@
 //module to process user input
+
+function processInput(input)
+{
+  if(input.length > 0)
+  {
+      //call method to display randomn word details
+      console.log("arguments array empty");
+  }
+  else if(input.length == 2 )
+  {
+      //call method for processing arguments and validation
+      console.log("arguments array size is 2");
+  }
+  else if(input.length == 1)
+  {
+      //check if argument if play and call the appropraite method
+      console.log("arguments array size is 1");
+  }
+}
