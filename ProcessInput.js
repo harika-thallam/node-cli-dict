ec736bd631e8055de00e8ed35cff4286c01fc05e
harika thallam <tharika70@gmail.com>
2020-03-02 07:13:58 +0530

    added to call to validate from processInput

--- ProcessInput.js
+++ ProcessInput.js
@@ -6,16 +6,19 @@ function processInput(input)
   if(input.length == 0)
   {
       //TODO:call method to display randomn word details
+      validate("randomn");
       console.log("arguments array empty");
   }
   else if(input.length == 2 )
   {
       //TODO:call method for processing arguments and validation
+      validate(input[0],input[1]);
       console.log("arguments array size is 2");
   }
   else if(input.length == 1)
   {
       //TODO:check if argument if play and call the appropraite method
+      validate(input[0]);
       console.log("arguments array size is 1");
   }
   else if(input.length > 2)
@@ -24,24 +27,31 @@ function processInput(input)
      console.error("invalid input");
   }
 }
-function validate(option,word)
+function validate(option,word="")
 {
   switch(option)
   {
     case "defn"://TODO:make call to receive definition of the entered word
+                console.log(`fetching definition of ${word}`);
                 break;
     case "syn"://TODO:make call to receive synonym of the entered word
+                console.log(`fetching synonym of ${word}`);
                 break;
     case "ant"://TODO:make call to receive antonym of the entered word
+                console.log(`fetching antonym of ${word}`);
                break;
     case "ex"://TODO:make call to receive example usage of the entered word
+               console.log(`fetching example of ${word}`);
                break;
     case "full"://TODO:make calls to display all the above information
-                break;
-    case "randomn"://TODO:make call to display details of randomn word
-                break;
+               console.log(`fetching information of ${word}`); 
+               break;
+    case "randomn"://TODO:make call to display details of randomn word     
+               console.log(`fetching details of randomn word`); 
+               break;
     case "play"://TODO:make calls to display details of the word and then guess the word
-                break;
+               console.log(`word play`); 
+               break;
   }
 }
 module.exports.processInput = processInput;
